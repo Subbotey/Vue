3d4335cf48d2ced21dc264627c66128929881942
Subbotey <subbotey@yandex.ru>
2020-07-12 15:48:28 +0300

    Shestaya domashka

---
+++ Homework_Vue_06/public/js/script.js
@@ -0,0 +1,283 @@
+// Первая задача и вторая задачи
+// Папку npm_modules не добавлял
+
+window.arrReel = [
+	{
+		title: 'Shimano Aerlex 10000 XSB',
+		img: 'https://spinningline.ru/images/products_photo_providers/00266000/00266307/00266307_11416_LARGE.jpg',
+		description: 'Чего в первую очередь ожидают от карповых снастей? Высокой мощности, так как эта рыба на вываживании оказывает сильное сопротивление. Корпус получился очень прочным, не боится воздействия соленой воды. В строении этих моделей эксперты компании Шимано воплотили множество своих разработок. Например, систему 2-Speed Oscillation. Это технология колебания шпули, за счет которой обеспечивается разная скорость вращения. А благодаря системе Aero Wrap 2 намотка лески получается ровной. Шпуля выполнена с V-образным краем. Поэтому повышается не только дальность заброса, но и его точность. Чтобы ход стал как можно более плавным, использована технология Dyna Balance System, устраняющая посторонние колебания. Модификация 10000 XSB весит 645 гр. Фрикционный тормоз рассчитан на нагрузку до 15 кг. Показатель передаточного числа составляет 4,6:1.',
+		art: 'ALX10000XSB',
+		manufacturer: 'Shimano',
+		weight: 645,
+		type: 'карповая',
+		price: '10 400',
+	},
+	{
+		title: 'Shimano Aerlex 10000 XTB',
+		img: 'https://spinningline.ru/images/products_photo_providers/00266000/00266308/00266308_11417_LARGE.jpg',
+		description: 'Чего ждут от карповых снастей в первую очередь? Конечно же, высокой прочности, так как карп на вываживании оказывает мощное сопротивление. «Тело» катушки становится очень прочным, не боится морской воды, поэтому такую модель можно использовать не только в пресном водоеме. В конструкциях этих катушек реализовано несколько систем, обеспечивающих большую производительность. Система Aero Wrap 2 делает намотку лески на шпулю более ровной. Сама шпуля имеет V-образный край. Такая форма делает заброс не только более точным, но и увеличивает его дальность. Данная модель, обозначенная индексом 10000 XTB, весит 645 гр. Показатель передаточного числа равен 4,6:1. Максимальная нагрузка на фрикцион может составлять до 15 кг.',
+		art: 'ALX10000XTB',
+		manufacturer: 'Shimano',
+		weight: 645,
+		type: 'карповая',
+		price: '7 546',
+	},
+	{
+		title: 'Shimano Aerlex 10000 XTB Spod',
+		img: 'https://spinningline.ru/images/products_photo_providers/00266000/00266309/00266309_11419_LARGE.jpg',
+		description: 'Карповые снасти должны обладать в первую очередь высокой надежностью, так как эта мирная рыба на вываживании оказывает существенное сопротивление. «Тело» Aerlex получилось очень прочным, не боится воздействия соленой воды. В строении данных катушек реализовано несколько разработок компании Шимано. Например, система Aero Wrap 2 делает намотку лески на шпулю ровнее. Балансировка Dyna Balance System устраняет посторонние колебания и делает ход максимально ровным и плавным. Эта сподовая модификация предназначена для прикармливания. Ее масса составляет 650 гр. Показатель передаточного числа равен 5,3:1. Фрикционный тормоз Aerlex 10000 XTB Spod рассчитан на нагрузку до 15 кг.',
+		art: 'ALX10000XTBSP',
+		manufacturer: 'Shimano',
+		weight: 650,
+		type: 'карповая',
+		price: '10 400',
+	},
+	{
+		title: 'Shimano Aero Technium 14000 XSC',
+		img: 'https://spinningline.ru/images/products_photo_providers/00266000/00266289/00266289_11360_LARGE.jpg',
+		description: 'Силовая катушка Aero Technium 14000 XSC Magnesium выполнена из материала CI4+. Благодаря его использованию удалось добиться для корпуса особенно высокой прочности. Такая катушка готова к экстремальным нагрузкам, которые возникают в карповой ловле, в морской, при охоте на трофейную рыбу. «Тело» катушки очень жесткое, что помогает снизить износ внутренних деталей. Ротор Magnumlite очень легкий, с его помощью обеспечивается плавное вращение, а нагрузка по нему распределяется равномерно, что предотвращает деформацию. Передаточный механизм Hagane Gearing отвечает за максимальную производительность катушки в целом, чтобы при силовом вываживании она работала как можно более эффективно. Весит данная модель 485 гр. Показатель передаточного числа составляет 4:3,1.',
+		art: 'ARTCMGS14000XSC',
+		manufacturer: 'Shimano',
+		weight: 485,
+		type: 'карповая',
+		price: '70 100',
+	},
+	{
+		title: 'Shimano Aero Technium 14000 XTC',
+		img: 'https://spinningline.ru/images/products_photo_providers/00266000/00266288/00266288_11356_LARGE.jpg',
+		description: 'Катушки серии Aero Technium наделены непревзойденной прочностью, которая позволяет им справляться с экстремальными нагрузками. Применять такую модель можно в карповой ловле, а также в других видах рыбалки, где от снастей требуются столь высокие тяговые характеристики. Для изготовления корпуса в данном случае используется материал CI4+. С его помощью удалось добиться очень высокой жёсткости, благодаря которой внутренние детали надежно защищаются от любых повреждений. Не менее надёжно механизм защищен и от попадания влаги. Ротор Magnumlite весит очень мало, нагрузка по нему распределяется равномерно, за счет чего удается избежать деформации. Масса модели Aero Technium 14000 XTC Magnesium составляет 485 гр. Показатель передаточного числа равен 4,3:1.',
+		art: 'ARTCMGS14000XTC',
+		manufacturer: 'Shimano',
+		weight: 485,
+		type: 'карповая',
+		price: '37 779',
+	},
+	{
+		title: 'Shimano Alivio 6000 FA',
+		img: 'https://spinningline.ru/images/products_photo_providers/00266000/00266282/00266282_11344_LARGE.jpg',
+		description: 'Модель безынерционной катушки Alivio 6000 FA предназначена для того, чтобы оснастить ею фидерное удилище. Форма шпули выполнена таким образом, чтобы заброс получился не только дальним, но и максимально точным. Каждая деталь внутреннего механизма Alivio продумана для как можно большей производительности. В конструкции катушки реализована система Super Stopper II. Она позволяет избежать ненужных проворотов рукояти в обратную сторону. Нежелательные вибрации при вращении устраняются при помощи системы Dyna Balance. Масса модели Alivio 6000 FA составляет 590 гр. Фрикционный тормоз рассчитан на нагрузку до 8 кг. Показатель передаточного числа в данном случае равен 4,9:1.',
+		art: 'ALV6000FA',
+		manufacturer: 'Shimano',
+		weight: 590,
+		type: 'донная/фидерная',
+		price: '5 130',
+	},
+]
+
+Vue.component('top-menu', {
+	props: ['arr-reel'],
+    template: `<div class="uk-width-3-4 uk-align-center" uk-grid>
+	    <ul uk-tab>
+	        <li class="uk-active">
+	            <router-link to='/' exact>Twin Power</router-link>
+	        </li>
+	        <li>
+	            <router-link to='/soare'>Soare CI4+</router-link>
+	        </li>
+	        <li>
+	            <router-link to='/stradic'>Stradic CI4</router-link>
+	        </li>
+	        <li>
+	            <router-link to='/list'>Список товаров</router-link>
+	        </li>
+	    </ul>
+	    <router-view :arrReel='arrReel' ></router-view>
+	</div>`,
+})
+
+const Twin = {
+    template: `<div class="uk-padding-remove-left uk-text-center uk-child-width-expand@m uk-grid-match uk-animation-fade" uk-grid>
+		<div class="uk-width-1-3@m">
+			<div class="uk-card uk-card-default uk-card-body">
+				<h3 class="uk-heading-divider uk-card-title">Катушка Shimano Twin Power C2000S</h3>
+				<p><img src="https://images.spinningline.ru/00178621_LARGE-001.jpg"></p>
+			</div>
+		</div>
+		<div>
+			<div class="uk-card uk-card-default uk-card-body uk-text-left">
+				<h3 class="uk-card-title uk-heading-divider">Характеристики</h3>
+				<div><span class="uk-text-bold">Описание:</span> Twin Power – это очень обширная линейка безынерционок бренда Shimano, в которую входят модификации с разными техническими характеристиками. Здесь представлена модель, предназначенная для оснащения ультралайтового удилища.</div>
+				<p v-if="visible">Вес C2000S составляет всего 193 гр. Фрикционный тормоз расположен в передней части корпуса и рассчитан на нагрузку до 3 кг. Передаточное отношение равняется 5,0:1. Для создания корпуса эксперты Shimano применили особый материал, наделенный высокой прочностью – CI4+. Система Coreprotect позволяет использовать эту катушку в морской ловле, защищая от разрушительного воздействия морской воды. Холоднокованый привод Hagane Gear обеспечивает бесперебойную стабильную работу катушек Twin Power. Каждая деталь этих моделей продумана экспертами Shimano для идеальной работы.</p>
+				<div><a @click="visible = !visible">{{visible?'Скрыть описание':'Подробнее'}}</a></div>
+				<p><span class="uk-text-bold">Артикул:</span> TPC2000S</p>
+				<p><span class="uk-text-bold">Производитель:</span> <a href="https://www.shimano.com/en/">Shimano</a></p>
+				<p><span class="uk-text-bold">Вес:</span> 193 гр.</p>
+				<p><span class="uk-text-bold">Класс катушки:</span> ultralight</p>
+				<p><span class="uk-text-bold">Количество подшипников:</span> 9+1</p>
+				<p><span class="uk-text-bold">Цена:</span> 32 060 руб.</p>
+			</div>
+		</div>
+	</div>`,
+    data() {
+        return {
+            visible: false,
+        }
+    }
+}
+
+const Soare = {
+	template: `<div class="uk-padding-remove-left uk-text-center uk-child-width-expand@m uk-grid-match uk-animation-fade" uk-grid>
+		<div class="uk-width-1-3@m">
+			<div class="uk-card uk-card-default uk-card-body">
+				<h3 class="uk-heading-divider uk-card-title">Катушка Shimano Soare CI4+ C2000SSPG</h3>
+				<p><img src="https://images.spinningline.ru/00180838_LARGE-001.jpg"></p>
+			</div>
+		</div>
+		<div>
+			<div class="uk-card uk-card-default uk-card-body uk-text-left">
+				<h3 class="uk-card-title uk-heading-divider">Характеристики</h3>
+				<div><span class="uk-text-bold">Описание:</span> Разработчики японского концерна Shimano в 2017 году обновили серию безынерционных катушек Soare CI4+. Эти катушки давно в ходу у любителей ультралайта и микроджига. Особой популярностью безынерционки пользуются у поклонников ловли форели на водоемах-платниках.</div>
+				<p v-if="visible">Также эта серия серия может использоваться для рокфишинга - лучше всего для этой цели подойдет модель индексом C2000SSPG. Для изготовления компактного корпуса G-Free Body использован материал CI4+. Именно за счет этого катушки такие легкие и надежные. У старших моделей для снижения веса установлен облегченный ротор Magnum Light Rotor. В их механизме установлена комбинация деталей X-Ship, а также антикоррозийная защита CoreProtect. У всех моделей реализована система антиреверса. Ротор может двигаться только в режиме намотки. Фрикцион расположен спереди. Тормозные диски защищены от попадания влаги специальной прокладкой. Фирменная шпуля со скошенным бортиком повышает дальность заброса за счет снижения трения. Рукоять катушки, которая также изготовлена из материала CI4+, можно переставить под другую руки.</p>
+				<div><a @click="visible = !visible">{{visible?'Скрыть описание':'Подробнее'}}</a></div>
+				<p><span class="uk-text-bold">Артикул:</span> CI4+C20SSPG</p>
+				<p><span class="uk-text-bold">Производитель:</span> <a href="https://www.shimano.com/en/">Shimano</a></p>
+				<p><span class="uk-text-bold">Вес:</span> 162 гр.</p>
+				<p><span class="uk-text-bold">Класс катушки:</span> ultralight</p>
+				<p><span class="uk-text-bold">Количество подшипников:</span> 9+1</p>
+				<p><span class="uk-text-bold">Цена:</span> 25 900 руб.</p>
+			</div>
+		</div>
+	</div>`,
+    data() {
+        return {
+            visible: false,
+        }
+    }
+}
+
+const Stradic = {
+	template: `<div class="uk-padding-remove-left uk-text-center uk-child-width-expand@m uk-grid-match uk-animation-fade" uk-grid>
+		<div class="uk-width-1-3@m">
+			<div class="uk-card uk-card-default uk-card-body">
+				<h3 class="uk-heading-divider uk-card-title">Катушка Shimano Stradic CI4+ C3000</h3>
+				<p><img src="https://images.spinningline.ru/00178266_LARGE-001.jpg"></p>
+			</div>
+		</div>
+		<div>
+			<div class="uk-card uk-card-default uk-card-body uk-text-left">
+				<h3 class="uk-card-title uk-heading-divider">Характеристики</h3>
+				<div><span class="uk-text-bold">Описание:</span> Легкие катушки серии Stradic CI4+ оснащены ротором Magnumlite Rotor. С его помощью удалось достичь очень плавного вращения, а также высокой чувствительности. Корпус G Free обладает минимальным весом, при этом его прочность на должном уровне.</div>
+				<p v-if="visible">Внутри корпуса скрыт шестереночный механизм X-Ship Hagane, обеспечивающий стабильную и бесперебойную работу катушки. Внутренние детали защищены от попадания воды при помощи системы Coreprotect. Фрикционный тормоз с передним расположением рассчитан на нагрузки до 9 кг. Применяться Stradic CI4+ могут как в спиннинговой ловле в пресной воде, так и в морской, так как их корпус и внутренние детали защищены от разрушительного воздействия соли.</p>
+				<div><a @click="visible = !visible">{{visible?'Скрыть описание':'Подробнее'}}</a></div>
+				<p><span class="uk-text-bold">Артикул:</span> STCI4-3C000FB</p>
+				<p><span class="uk-text-bold">Производитель:</span> <a href="https://www.shimano.com/en/">Shimano</a></p>
+				<p><span class="uk-text-bold">Вес:</span> 193 гр.</p>
+				<p><span class="uk-text-bold">Класс катушки:</span> medium, light</p>
+				<p><span class="uk-text-bold">Количество подшипников:</span> 6 S A-RB+1RB</p>
+				<p><span class="uk-text-bold">Цена:</span> 19 800 руб.</p>
+			</div>
+		</div>
+	</div>`,
+    data() {
+        return {
+            visible: false,
+        }
+    }
+}
+
+const NotFound = {
+	template:'<h2>Страница отсутствует</h2>'
+}
+
+const List = {
+	props: ['arrReel'],
+	template: `<div class="uk-align-center uk-animation-fade" uk-grid>
+	    <table class="uk-table uk-table-justify uk-table-divider">
+	        <tbody>
+	            <tr v-for="(reel, i) in arrReel" :key="reel.id">
+	                <td>
+	                    <img class="uk-height-small" :src="arrReel[i].img">
+	                </td>
+	                <td class=" uk-text-middle">{{arrReel[i].title}}</td>
+	                <td class=" uk-text-middle">Цена: {{arrReel[i].price}}</td>
+	                <td class=" uk-text-middle"><button :id='i' @click.prevent="getProduct(i)" class="uk-button uk-button-default">Подробнее</button></td>
+	            </tr>
+	        </tbody>
+	    </table>
+	</div>`,
+	methods:{
+		getProduct(i) {
+			this.$router.push('/list/' + i)
+		}
+	}
+
+}
+
+const Product = {
+	props: ['arr-reel'],
+  template: `<div class="uk-padding-remove-left uk-text-center uk-child-width-expand@m uk-grid-match uk-animation-fade" uk-grid>
+      <div class="uk-width-1-3@m">
+          <div class="uk-card uk-card-default uk-card-body">
+              <h3 class="uk-heading-divider uk-card-title">{{arrReel[$route.params.id].title}}</h3>
+              <p><img :src="arrReel[$route.params.id].img"></p>
+          </div>
+      </div>
+      <div>
+          <div class="uk-card uk-card-default uk-card-body uk-text-left ">
+              <h3 class="uk-card-title uk-heading-divider">Характеристики</h3>
+              <div><span class="uk-text-bold">Описание:</span> {{arrReel[$route.params.id].description}}</div>
+              <p><span class="uk-text-bold">Артикул:</span> {{arrReel[$route.params.id].art}}</p>
+              <p><span class="uk-text-bold">Производитель:</span> <a :href="arrReel[$route.params.id].link">{{arrReel[$route.params.id].manufacturer}}</a></p>
+              <p><span class="uk-text-bold">Вес:</span> {{arrReel[$route.params.id].weight | weightGr}}</p>
+              <p><span class="uk-text-bold">Класс катушки:</span> {{arrReel[$route.params.id].class}}</p>
+              <p><span class="uk-text-bold">Цена:</span> {{arrReel[$route.params.id].price | formatPrice}}</p>
+          </div>
+      </div>
+  </div>
+
+  <div>Пользователь {{ arrReel[$route.params.id].art | weightGr}}</div>`,
+  filters: {
+	  formatPrice: function(value) {
+		  return value + ' ' + String.fromCharCode(8381);
+	  },
+	  weightGr: function(value) {
+		  return value + ' гр.';
+	  }
+  }
+}
+
+const routes = [
+    {
+        path: '/soare',
+        component: Soare
+    },
+    {
+        path: '/list',
+        component: List
+    },
+    {
+        path: '/stradic',
+        component: Stradic
+    },
+    {
+        path: '/',
+        component: Twin
+    },
+	{
+		path: '/list/:id',
+		component: Product },
+	{
+		path: '*',
+		redirect: '/404'
+	},
+	{
+		path: '/404',
+		component: NotFound
+	}
+]
+
+const router = new VueRouter({
+	mode: 'history',
+    routes
+})
+
+new Vue({
+    router,
+	data:{
+		arrReel:[],
+		product:{},
+	},
+	created() {
+		this.arrReel = window.arrReel
+	},
+}).$mount('#app')
